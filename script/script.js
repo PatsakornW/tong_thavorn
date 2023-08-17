b4561b91b731f38e308723f6303baedfd4ed29fa
PatsakornW <patsakorn.wtnsb@gmail.com>
2023-08-17 23:09:37 +0700

    first_commit

---
+++ script/script.js
@@ -0,0 +1,56 @@
+/****** Add to Cart Number ******/
+$(document).ready(function() {
+    var thumb = $("#thumb");
+    var slidesPerPage = 4; //globaly define number of elements per page
+    var syncedSecondary = true;
+
+
+
+        $(".qtyminus").on("click",function(){
+            var now = $(".qty").val();
+            if ($.isNumeric(now)){
+                if (parseInt(now) -1> 0)
+                { now--;}
+                $(".qty").val(now);
+            }
+        })            
+        $(".qtyplus").on("click",function(){
+            var now = $(".qty").val();
+            if ($.isNumeric(now)){
+                $(".qty").val(parseInt(now)+1);
+            }
+        });
+});
+
+
+
+/******* Slide Image Product ******/
+const imgs = document.querySelectorAll('.img-select a');
+const imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
+
+function slideImage(){
+    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+}
+
+window.addEventListener('resize', slideImage);
+
+
+
+
+
+  $('#myCarousel').carousel({
+    interval: 5000
+  })
+  
+  
